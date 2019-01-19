fef6fd4faeaaad153a43453cf09f811d072f2bd9
Mahalia Henry-Richards <mahalia_hr@outlook.com>
2019-01-19 18:20:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key) {
 Object.assign({}, object)
 delete object.key
-return object
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
